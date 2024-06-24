ac87e0fc19d627568bef2b35474b81977a3b9910
Victor <victor.lins@treetech.com.br>
2024-06-24 13:25:00 -0300

    remove headers

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,12 +2,5 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    "Access-Control-Allow-Headers": "X-CSFR-Token,X-Requested-With, content-type, Authorization",
-  },
   withCredentials: true,
 })
