c96f73b6c241c7f774dd0f6c3d703695e2a2ff99
Victor <victor.lins@treetech.com.br>
2024-06-24 00:04:28 -0300

    add control origins

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,9 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
   withCredentials: true,
 })
