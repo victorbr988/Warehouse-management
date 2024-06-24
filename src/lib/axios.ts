1448feae3a43b4008ab7f618d5cd53f8b372f22c
Victor <victor.lins@treetech.com.br>
2024-06-24 12:49:02 -0300

    add headers access controls

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,7 +4,10 @@ export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Allow-Headers": "X-CSFR-Token,X-Requested-With, content-type, Authorization",
   },
   withCredentials: true,
 })
